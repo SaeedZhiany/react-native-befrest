173322c4e6fab6107a10491865a549edd2d49467
SAEED <developer.saeedzhiany@gmail.com>
2019-02-06 18:30:11 +0330

    export the library methods to access from react-native side

--- index.js
+++ index.js
@@ -1,6 +1,53 @@
+import {NativeModules} from 'react-native';
 
-import { NativeModules } from 'react-native';
+const {ReactNativeBefrest} = NativeModules;
 
-const { ReactNativeBefrest } = NativeModules;
-
-export default ReactNativeBefrest;
+export default {
+  init(
+    uId: number,
+    auth: string,
+    chId: string
+  ) {
+    ReactNativeBefrest.init(uId, auth, chId);
+  },
+  setUId(
+    uId: number
+  ) {
+    ReactNativeBefrest.setUId(uId);
+  },
+  setChId(
+    chId: number
+  ) {
+    ReactNativeBefrest.setChId(chId);
+  },
+  setAuth(
+    auth: string
+  ) {
+    ReactNativeBefrest.setAuth(auth);
+  },
+  start() {
+    ReactNativeBefrest.start();
+  },
+  stop() {
+    ReactNativeBefrest.stop();
+  },
+  addTopic(
+    topic: string
+  ) {
+    ReactNativeBefrest.addTopic(topic);
+  },
+  removeTopic(
+    topic: string
+  ) {
+    ReactNativeBefrest.removeTopic(topic);
+  },
+  getCurrentTopics(): Promise<string[]>{
+    return ReactNativeBefrest.getCurrentTopics();
+  },
+  refresh(): Promise<boolean>{
+    return ReactNativeBefrest.refresh();
+  },
+  getSdkVersion(): Promise<number>{
+    return ReactNativeBefrest.getSdkVersion();
+  }
+};
