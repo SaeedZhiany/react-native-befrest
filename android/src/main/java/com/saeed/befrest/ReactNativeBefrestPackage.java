ebccda6c58fa0df45aa73cd6261df40d9d751372
SAEED <developer.saeedzhiany@gmail.com>
2019-02-06 18:25:25 +0330

    used singletonList instead of asList method and fix indention of code

--- android/src/main/java/com/saeed/befrest/ReactNativeBefrestPackage.java
+++ android/src/main/java/com/saeed/befrest/ReactNativeBefrestPackage.java
@@ -1,28 +1,28 @@
 
 package com.saeed.befrest;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
-import com.facebook.react.bridge.JavaScriptModule;
+
+import java.util.Collections;
+import java.util.List;
+
 public class ReactNativeBefrestPackage implements ReactPackage {
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-      return Arrays.<NativeModule>asList(new ReactNativeBefrestModule(reactContext));
+        return Collections.<NativeModule>singletonList(new ReactNativeBefrestModule(reactContext));
     }
 
     @SuppressWarnings("unused")
     public List<Class<? extends JavaScriptModule>> createJSModules() {
-      return Collections.emptyList();
+        return Collections.emptyList();
     }
 
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
-      return Collections.emptyList();
+        return Collections.emptyList();
     }
 }
